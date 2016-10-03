e8f482f084b86154b0a32577a2c6bb583e7df4f8
micahpotts <mrs.micahpotts@gmail.com>
2016-10-03 15:17:20 -0400

    Set buttons to highlight on click

--- js/flickr.js
+++ js/flickr.js
@@ -3,3 +3,10 @@
 //make a GET request to flickr, sending the search word along
 //receive JSON response
 //Add a link and an <img> tag to the page
+
+$(document).ready(function() {
+	$('button').click(function() {
+		$('button').removeClass('selected');
+		$(this).addClass('selected');
+	});
+}); //end ready
