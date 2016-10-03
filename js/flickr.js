8a7918a98a8a417f0f81732f61c8039fcda6e5e8
micahpotts <mrs.micahpotts@gmail.com>
2016-10-03 15:01:43 -0400

    Added js file. Planned steps to complete project.

---
+++ js/flickr.js
@@ -0,0 +1,5 @@
+//User Clicks button
+//JS program will get the word on that button
+//make a GET request to flickr, sending the search word along
+//receive JSON response
+//Add a link and an <img> tag to the page
